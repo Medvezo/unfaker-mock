2703fe7425b56a3f7704368d41612aa74f512fe5
Medvezo <mansur.mustafayev@gmail.com>
2023-12-02 18:42:12 +0400

    Add custom Password Input component

---
+++ src/components/form/PasswordInput.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { Input } from "@nextui-org/input";
+
+type PasswordInputType = {
+	value: any;
+	setValue: any;
+};
+
+export default function PasswordInput({ value, setValue }: PasswordInputType) {
+	// Password visibility
+	const [isVisible, setIsVisible] = useState(false);
+	const toggleVisibility = () => setIsVisible(!isVisible);
+
+	return (
+		<Input
+			isRequired
+			endContent={
+				<button
+					className="focus:outline-none"
+					type="button"
+					onClick={toggleVisibility}
+				>
+					{isVisible ? (
+						<AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
+					) : (
+						<AiOutlineEye className="text-2xl text-default-400 pointer-events-none" />
+					)}
+				</button>
+			}
+			type={isVisible ? "text" : "password"}
+			variant="underlined"
+			label="Password"
+			value={value.password}
+			labelPlacement="outside"
+			onChange={(e: any) => setValue({ ...value, password: e.target.value })}
+		/>
+	);
+}

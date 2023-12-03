6ce9112ea17d95d45a4d2387eed90bf2f63bea88
Medvezo <mansur.mustafayev@gmail.com>
2023-12-03 16:06:27 +0400

    Handle Submit Analyze in global store

--- src/components/form/VideoDropzone.tsx
+++ src/components/form/VideoDropzone.tsx
@@ -1,11 +1,15 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Button } from "@nextui-org/button";
 import { TiDeleteOutline } from "react-icons/ti";
+import { useVideo } from "@/components/container/VideoProvider";
 
 export default function VideoDropzone() {
+	const [buttonDisabled, setButtonDisabled] = useState(true);
 	const [files, setFiles] = useState<File[]>([]);
+	// Global store state
+	const { setVideoFile } = useVideo();
 
 	// Ondropo handle
 	const onDrop = useCallback((acceptedFiles: File[]) => {
@@ -29,6 +33,17 @@ export default function VideoDropzone() {
 		maxSize: 30000000,
 	});
 
+	// Button disabled handle
+	useEffect(() => {
+		files.length > 0 ? setButtonDisabled(false) : setButtonDisabled(true);
+	}, [files.length]);
+
+	// Handle Submit
+	const handleSubmit = () => {
+		setVideoFile(files[0]);
+		console.log(files[0]);
+	};
+
 	return (
 		<div className="container mx-auto max-w-sm sm:max-w-xl lg:max-w-6xl px-6 lg:px-12">
 			<div
@@ -43,7 +58,7 @@ export default function VideoDropzone() {
 					30MB)
 				</em>
 			</div>
-			<aside className="mt-4">
+			<aside className="mt-4 w-full flex flex-col justify-center items-center gap-5">
 				<h4 className="font-semibold text-lg">File:</h4>
 				<ul className="flex flex-wrap justify-center items-center gap-10">
 					{files.map((file) => (
@@ -51,7 +66,10 @@ export default function VideoDropzone() {
 							key={file.name}
 							className="mt-2 flex justify-between items-center ">
 							<div className="relative ">
-								{file.name} - {(file.size / 1024 / 1024).toFixed(2)} MB
+								<p className="font-bold  mb-5 underline underline-offset-8">
+									{file.name} -{" "}
+									<em>{(file.size / 1024 / 1024).toFixed(2)} MB</em>
+								</p>
 								<video className="mt-2 w-full max-w-xs " controls>
 									<source src={URL.createObjectURL(file)} type="video/mp4" />
 									Your browser does not support the video tag.
@@ -69,6 +87,14 @@ export default function VideoDropzone() {
 						</li>
 					))}
 				</ul>
+				<Button
+					isDisabled={buttonDisabled}
+					onClick={handleSubmit}
+					color="success"
+					variant="solid"
+					className=" w-full mt-10">
+					Analyze
+				</Button>
 			</aside>
 		</div>
 	);
